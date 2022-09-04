513871873aaaf1f6cc003fee479ac906f935ba56
Dhiraj G <gdhiraj1996@gmail.com>
2022-09-04 07:38:14 +0000

    Sync LeetCode submission - Valid Palindrome (python3)

---
+++ problems/valid_palindrome/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s=s.lower()
+        s=''.join(i for i in s if i.isalnum())
+        if s==''.join(reversed(s)):
+            return True
+        else:
+            return False
