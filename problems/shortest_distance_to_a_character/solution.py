7e3262d3280afe371c2784ad824bc598a6c63281
Dhiraj G <gdhiraj1996@gmail.com>
2023-06-22 06:45:31 +0000

    Sync LeetCode submission - Shortest Distance to a Character (python3)

---
+++ problems/shortest_distance_to_a_character/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def shortestToChar(self, s: str, c: str) -> List[int]:
+        N = len(s)
+        left, right, output = [None] * N, [None] * N, [None] * N
+        tmp = float("inf")
+        for i in range(N):
+            if c == s[i]:
+                tmp = 0
+            left[i] = tmp
+            tmp+=1
+        tmp = float("inf")
+        for i in range(N-1,-1,-1):
+            if c == s[i]:
+                tmp = 0
+            right[i] = tmp
+            tmp+=1
+        for i in range(N):
+            output[i] = min(right[i], left[i])
+        return output
+
+
