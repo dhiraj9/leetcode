44efb74d982170439ef4ff33e06b08868467349f
Dhiraj G <gdhiraj1996@gmail.com>
2023-07-23 13:06:03 +0000

    Sync LeetCode submission - First Bad Version (python3)

---
+++ problems/first_bad_version/solution.py
@@ -0,0 +1,17 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        s = 1
+        e = n
+        while s < e:
+            m = (s + e) // 2
+            if isBadVersion(m):
+                e = m
+            else:
+                s = m + 1
+        return s
+
+        
+
