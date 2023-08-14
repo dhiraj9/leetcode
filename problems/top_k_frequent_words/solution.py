a0bbc88de0c506d9e9768c6b422773e747676f24
Dhiraj G <gdhiraj1996@gmail.com>
2023-08-14 03:22:48 +0000

    Sync LeetCode submission - Top K Frequent Words (python3)

--- problems/top_k_frequent_words/solution.py
+++ problems/top_k_frequent_words/solution.py
@@ -1,4 +1,14 @@
+import heapq
+
+
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
         wordmap = Counter(words)
-        return sorted(wordmap, key=lambda x: (-wordmap[x], x))[:k]
+        minheap = [(-freq, word) for word, freq in wordmap.items()]
+        heapq.heapify(minheap)
+        result = []
+        while k>0:
+            result.append(heappop(minheap)[1])
+            k -= 1
+        return result
+
