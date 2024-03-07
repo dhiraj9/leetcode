9c8522825461c264096a7c3ddcc7ddfa5fc207fb
Dhiraj G <dgunashe@buffalo.edu>
2024-03-07 14:06:04 -0500

    Time: 22 ms (35.45%) | Memory: 45.5 MB (9.86%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -3,18 +3,19 @@ class Solution {
         int n = s.length();
         boolean[][] dp = new boolean[n][n];
         int res = 0;
-        for (int i = 0; i < n; i++) {
-            dp[i][i] = true;
-            res++;
-        }
-        for (int i = 0; i < n - 1; i++) {
-            dp[i][i + 1] = s.charAt(i) == s.charAt(i + 1);
-            if (dp[i][i + 1]) res++;
-        }
-        for (int len = 3; len <= n; len++) {
+        for (int len = 1; len <= n; len++) {
             for (int i = 0, j = i + len - 1; j < n; i++, j++) {
-                dp[i][j] = s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1];
-                if (dp[i][j]) res++;
+                if (len == 1) {
+                    dp[i][i] = true;
+                    res++;
+                } else if (len == 2) {
+                    dp[i][i + 1] = s.charAt(i) == s.charAt(i + 1);
+                    if (dp[i][i + 1]) res++;
+                }
+                else {
+                    dp[i][j] = s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1];
+                    if (dp[i][j]) res++;
+                }
             }
         }
         return res;
