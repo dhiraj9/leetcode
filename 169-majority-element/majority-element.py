68784b31f983f129480314558978e04df8e15839
Dhiraj G <dgunashe@buffalo.edu>
2024-03-31 16:48:49 -0400

    Time: 161 ms (48.55%) | Memory: 18.1 MB (35.12%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -1,7 +1,10 @@
+import collections
+import math
+
+
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        a = collections.Counter(nums)
-        b = math.ceil(len(nums) / 2)
-        for i, j in a.items():
-            if j >= b:
-                return i
+        return collections.Counter(nums).most_common()[0][0]
+
+
+
