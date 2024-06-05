77392e564c1c140770f69db19f53dea06ac85fe1
Dhiraj G <dgunashe@buffalo.edu>
2024-06-05 04:45:03 -0400

    Time: 80 ms (24.93%) | Memory: 19.8 MB (72.25%) - LeetSync

--- 57-insert-interval/insert-interval.py
+++ 57-insert-interval/insert-interval.py
@@ -11,7 +11,3 @@ class Solution:
                 newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
         res.append(newInterval)
         return res
-
-
-
-
